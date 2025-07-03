de287080a1bcfe5b1260d51f8d70ca6961bc046b
unknown <ahmedatefromiah@gmail.com>
2025-07-03 10:36:49 +0300

    add execption translation

--- shared/Filters/ExceptionFilter.ts
+++ shared/Filters/ExceptionFilter.ts
@@ -19,13 +19,20 @@ export class ResponseExceptionFilter implements ExceptionFilter {
   catch(exception: any, host: ArgumentsHost) {
     const context = host.switchToHttp();
     const response = context.getResponse<Response>();
-    const req    = context.getRequest<Request>();
-    const lang   = (req.headers['accept-language'] as string) || 'en';
+    const req = context.getRequest<Request>();
+    const lang = (req.headers['accept-language'] as string) || 'en';
     try {
       var obj = {};
 
       exception['response']['message'].map((e) => {
-        obj[this.i18n.translate(`common.${e['property']}` , {lang:lang}) as string] = this.i18n.translate(`common.${Object.values(e['constraints'])[0]}` , {lang});
+        obj[
+          this.i18n.translate(`common.${e['property']}`, {
+            lang: lang,
+          }) as string
+        ] = this.i18n.translate(
+          `common.${Object.values(e['constraints'])[0]}`,
+          { lang },
+        );
       });
       const errorResponse = {
         code: Code.BAD_INPUT,
@@ -33,27 +40,34 @@ export class ResponseExceptionFilter implements ExceptionFilter {
       } as ServiceResponse<Object>;
       return response.status(400).json(errorResponse);
     } catch {
-      console.log(exception)
+      console.log(exception);
       if (exception instanceof QueryFailedError) {
         return response.status(400).json({ code: Code.SMTH_WITH_DB });
       }
     }
     if (exception instanceof NotFoundException) {
-      return response.status(404).json({ code: Code.NOT_FOUND });
+      return response
+        .status(404)
+        .json({ code: this.i18n.translate(`common.${Code.NOT_FOUND}`) });
     }
-    if(exception instanceof ForbiddenException){
-      return response.status(403).json(()=>{
-        if(exception.message != Code.UN_VERIFIED){
-        return response.status(403).json ({Code:Code.FORBIDDEN})
+    if (exception instanceof ForbiddenException) {
+      return response.status(403).json(() => {
+        if (exception.message != Code.UN_VERIFIED) {
+          return response.status(403).json({ Code: Code.FORBIDDEN });
+        } else {
+          return response.status(403).json({ Code: Code.UN_VERIFIED });
         }
-        else{
-          return response.status(403).json ({Code:Code.UN_VERIFIED})
-        }
-      })
+      });
     }
-    if(exception instanceof UnauthorizedException){
-      return response.status(401).json({Code:Code.UN_AUTORIZED})
+    if (exception instanceof UnauthorizedException) {
+      return response.status(401).json({ Code: Code.UN_AUTORIZED });
     }
-    return response.status(400).json({ code: this.i18n.translate(`common.${exception.message}` , {lang:lang}) });
+    return response
+      .status(400)
+      .json({
+        code: this.i18n.translate(`common.${exception.message}`, {
+          lang: lang,
+        }),
+      });
   }
 }
